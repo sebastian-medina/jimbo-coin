04f8f2a54baf8d97565d18ea514961f1178f90e8
sebastian-medina <sebastian.medina@chartboost.com>
2021-12-04 18:28:15 +0100

    adding proof-of-work to blockchain

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# jimbo-coin
+# Blockchain & Jimbo Coin Excercise
+
+This project consists in a creating a blockchain - contains the following:
+ - proof-of-work algorithm
+ - verify blokchain integrity (prevent hacking)
+ - generate wallet for transactions
+ - signing transactions
+
+# Intro
+
+A blockchain is a block of chains that contains and records information in a way that makes it extremly diffuclt (almost impossible) to hack or cheat.
+
+It works as a digital ledger of transactions that's duplicated and distributed across the entire network of computer systems on the blockchain.
+
+Each block in the chain has a number of transactions, and every time a mew transaction takes place, a record of that transaction is added to every participant's ledger. And, contains data, hash, and the previous hash (a has is the unique identifier from the block)
+
+Each blockchain has a Proof-of-work. It's a mechanism that slows down the creation of new blocks.
+
+Whenever someone joins the network a block is created and everyone from the network gets a copy of that block (each node verifies the block to make sure it hasn't been changed) - creating consensus.
--- main.js
+++ main.js
@@ -10,20 +10,32 @@ class Block{
         this.previousHash = previousHash;
         // this property will be calculated (hash)
         this.hash = this.calculateHash();
+        // this has nothing to do with the block just to stop the while loop of mining the block so it stops
+        this.nonce = 0;
     }
 
     // new method to calculate new hash
     calculateHash(){
         // crypto js to import
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+    // proof-of-work (mining) - difficulty
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("block mined: ", this.hash);
     }
 }
 
 // new class for blockchain
 class Blockchain{
     constructor(){
-        this.chain = [this.createGenesisBlock()]; // area of blocks
         // add genesis block (initial 1st block in the blockchain)
+        this.chain = [this.createGenesisBlock()]; // area of blocks
+        // controls speed & nr of 0s to mine the block - the greater the difficulty the more time it takes
+        this.difficulty = 4;
     }
 
     createGenesisBlock(){
@@ -39,7 +51,9 @@ class Blockchain{
         // recalculate hash
         // append to chain
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        // newBlock.hash = newBlock.calculateHash();
+        // for the mine block
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
     // validate integrity of blockchain
@@ -64,18 +78,20 @@ class Blockchain{
 
 // test chain by creating an instance of the blockchain
 let jimboCoin = new Blockchain();
+console.log('mining block 1...');
 jimboCoin.addBlock(new Block(1, "03/11/2021", { amount: 4 }));
+console.log('mining block 2...');
 jimboCoin.addBlock(new Block(2, "05/11/2021", { amount: 7 }));
 
 // check how blockchain looks like
 // console.log(JSON.stringify(jimboCoin, null, 4));
 
-// check if blockchain is valid
-console.log('Is Blockchain valid? ' + jimboCoin.isChainValid());
+// // check if blockchain is valid
+// console.log('Is Blockchain valid? ' + jimboCoin.isChainValid());
 
-// try changing a block from the chain to see if it's valid
-jimboCoin.chain[1].data = { amount: 20 };
-// change integrity by recalculating hash
-jimboCoin.chain[1].hash = jimboCoin.chain[1].calculateHash();
-// concl block chain is meant to add new blocks but never change an existing block
-console.log('Is Blockchain valid? ' + jimboCoin.isChainValid());
+// // try changing a block from the chain to see if it's valid
+// jimboCoin.chain[1].data = { amount: 20 };
+// // change integrity by recalculating hash
+// jimboCoin.chain[1].hash = jimboCoin.chain[1].calculateHash();
+// // concl block chain is meant to add new blocks but never change an existing block
+// console.log('Is Blockchain valid? ' + jimboCoin.isChainValid());
